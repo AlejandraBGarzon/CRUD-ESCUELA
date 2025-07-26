2448e13c27f166aa9d996800340d651a505ff786
CrisAlejo316 <criss16aviila@gmail.com>
2025-07-25 21:34:58 -0500

    Ajustes de archivos y organización de carpetas del proyecto

--- frontend/service/escuela-patinaje.js
+++ frontend/service/escuela-patinaje.js
@@ -1,56 +1,4 @@
 // =======================================
-// =============== LOGIN =================
-// =======================================
-
-// // Esta función se ejecuta apenas se carga la página
-// window.onload = function () {
-//     // Revisamos si hay datos guardados en localStorage
-//     const usuarioGuardado = localStorage.getItem("usuario");
-//     const claveGuardada = localStorage.getItem("clave");
-
-//     // Si existen, los colocamos automáticamente en los campos de entrada
-//     if (usuarioGuardado && claveGuardada) {
-//         document.getElementById('nombre').value = usuarioGuardado;
-//         document.getElementById('clave').value = claveGuardada;
-//     }
-// };
-
-// function validar() {
-//     // Obtenemos los valores que el usuario ingresó en los campos
-//     const usuario = document.getElementById('nombre').value.trim();
-//     const clave = document.getElementById('clave').value.trim();
-
-//     // Validar si los campos están vacíos
-//     if (usuario === "" || clave === "") {
-//         alert("Por favor ingresa tu usuario y clave para continuar.");
-//         return; // Detiene la función
-//     }
-
-//     // Convertimos el usuario a minúsculas para comparar sin importar si escribió Ale o ale, etc.
-//     const usuarioLower = usuario.toLowerCase();
-
-//     // Validamos credenciales permitidas
-//     const credencialesValidas = (
-//         (usuarioLower === "ale" && clave === "ber123") ||
-//         (usuarioLower === "cristian" && clave === "cris@90") ||
-//         (usuarioLower === "leidy" && clave === "Le1dy@09") ||
-//         (usuarioLower === "jesica" && clave === "j123")
-//     );
-
-//     if (credencialesValidas) {
-//         // Guardar usuario y clave en localStorage
-//         localStorage.setItem("usuario", usuario);
-//         localStorage.setItem("clave", clave);
-
-//         // Redirigir a la página de información
-//         window.location.href = "#"; // Se cambia al tener html de segunda página
-//     } else {
-//         alert("Usuario o clave incorrectos. Intenta nuevamente.");
-//     }
-// }
-
-
-// =======================================
 // =============== CRUD ==================
 // =======================================
 
@@ -229,7 +177,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const edad = Number(edadInput.value.trim());
         const categoria = categoriaInput.value.trim();
 
-        const soloLetras = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+        const soloLetras = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s-]+$/;
 
         if (!nombre || !apellido || !edad || !categoria) {
             return alert('Por favor, completa todos los campos.');
@@ -347,7 +295,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             };
 
-            const soloLetras = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+            const soloLetras = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s-]+$/;
 
             if (!nuevo.nombre || !nuevo.apellido || !nuevo.edad || !nuevo.categoria) {
                 return alert('Por favor, completa todos los campos.');
